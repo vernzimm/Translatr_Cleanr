7922ba85b6d5d76242e9f17f8029039bd912ddf4
vernzimm <vern.zimm@gmail.com>
2019-06-22 15:30:30 -0400

    Keep ending punc if most strings have it. Give up on '&' if don't find corresponding ';', or if not matching known fix. Add ending punc list and separate function for '&;' fix to easily add/edit.

--- README.md
+++ README.md
@@ -4,14 +4,13 @@
 
 Nice little website that (Google) translates multiple languages at once. Doesn't do to well with some punctuation though. This is a little script to do several things...
   - Convert the various formatting (incomplete list, only those I have encountered)
-  - Strip ending punctuation that Google occasionally invents
+  - Strip ending punctuation that Google occasionally invents if it's not present on >1/2 of the strings
   - Output separate lines so we can triple-click or the like
 
 Creates "in.txt" and "out.txt" in current working dir
   - Copy-paste translated results into "in.txt"
   - "in.txt" will be cleared after writing to "out.txt"
-  - Loops every second, looking for fresh text saved into "in.txt" (will append "out.txt" for a mini-history)
+  - Loops looking for fresh text saved into "in.txt" (will append "out.txt" for a mini-history)
 
 Other Notes:
   - Translatr has an API... might be useful
-  - Doesn't respect original text that _does_ have ending punctuation
--- Translatr_Cleanr.py
+++ Translatr_Cleanr.py
@@ -1,60 +1,99 @@
+#list of sentence-ending punctuation to check for (to keep on all lines if found for each line, or delete on all if not)
+punclist = ['.','?','!','。','？','！']
+
+#list of formatting problems from Translatr to fix
+def fixfunc(fix):
+	if fix == '&#39':
+		fix = '\''
+	elif fix == '&quot':
+		fix = '\'\''
+	return(fix)
+
+#start of main program
 import os
 import time
 
 dir = os.getcwd()
 txtin = dir + '/in.txt'
 txtout = dir + '/out.txt'
+b = 'init'
+loop = False
+newloop = False
 
 while 0 == 0:
 
-	time.sleep(1)
-
-	loop = False
+	time.sleep(0.2)
 
 	try:
+		#check if txtin contains some text
 		if os.stat(txtin).st_size > 0:
 			loop = True
 			newloop = True
 	except:
+		#except if it doesn't even exist yet, make it
 		a = open(txtin, mode = 'w')
 		a.close()
 
-	while loop:
-		if newloop:
-			a = open(txtin, mode = 'r', encoding='utf-8')
-			z = open(txtout, mode = 'a', encoding='utf-8')
-			newloop = False
+	if newloop:
+		#init section that I'm probably doing in some grossly inefficient way like the rest of this program haha
+		a = open(txtin, mode = 'r', encoding='utf-8')
+		z = open(txtout, mode = 'a', encoding='utf-8')
+		#seems easier to read it twice than look up how to do "readline" on a string? I agree.
+		b = open(txtin, mode = 'r', encoding='utf-8').read()
+		#count ending punctuation and newlines, and set lambda for appropriate action.
+		punccnt = 0
+		nlin = 0
+		for char in b:
+			if any(char == punc for punc in punclist):
+				punccnt += 1
+		for char in b:
+			if char == '\n':
+				nlin += 1
+		if punccnt > nlin / 2:
+			trim = lambda: x == '\n'
+		else:
+			trim = lambda: any(x == punc for punc in punclist) or x == '\n'
+		newloop = False
 
+	while loop:
+		#again, probably grossly inefficient... non-judgemental comments are welcome.
 		b = a.readline()
 		if b == '':
 			z.write('----------------------------------------------------------------------------------\n\n')
 			z.close()
 			a.close()
+			#make new txtin file (delete the original contents)
 			a = open(txtin, mode = 'w')
 			a.close
+			loop = False
 			print('done')
 			break
 
+		#use our lambda to trim (at least) newline, and (if not consistent) line ending punctuation
 		x = b[-1]
-		while x == "." or x == '。' or x == '\n':
+		while trim():
 			b = b[:-1]
 			x = b[-1]
+
 		country = b.split(sep=':')[0] + ':'
 		b = b[len(country) + 1:]
 		z.write(country + '\n')
+
 		pile = ''
 		g = -1
 		for e in range(len(b)):
 			if b[e] == '&':
+				#look for & and then look for ;... if we didn't find ; within a few chars, forget about & and continue
+				#even if we did find ;, if it doesn't match in fixfunc it will return as is
 				f = ''
 				g = e
-				while b[g] != ';':
+				while b[g] != ';' and len(f) < 6:
 					f += b[g]
 					g += 1
-				if f == '&#39':
-					pile += '\''
-				elif f == '&quot':
-					pile += '\'\''
+				if len(f) < 6:
+					pile += fixfunc(f)
+				else:
+					pile += b[e]
 			elif g >= e :
 				pass
 			else:
